6a849e2a021025348e21b770f93b37e66d5844c5
vrijraj <vrijraj2396@gmail.com>
2023-09-01 22:47:47 +0530

    vue config updated

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,28 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  css: {
+    extract: { ignoreOrder: true },
+  },
+  configureWebpack: {
+    output: {
+      crossOriginLoading: "anonymous",
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch");
+  },
+  productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
-  ]
-})
+  ],
+  pwa: {
+    name: "DevFest India 2023",
+    workboxPluginMode: "InjectManifest",
+    themeColor: "#3D7FF5",
+    msTileColor: "#3D7FF5",
+    appleMobileWebAppCapable: "yes",
+    appleMobileWebAppStatusBarStyle: "black",
+    manifestOptions: {
+      background_color: "#ffffff",
+    },
+  })

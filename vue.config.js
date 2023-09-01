aaa1ba1baf269fbdf4986ce5b752e805760e70c6
vrijraj <vrijraj2396@gmail.com>
2023-09-01 23:06:29 +0530

    pwa removed

--- vue.config.js
+++ vue.config.js
@@ -3,16 +3,5 @@ module.exports = defineConfig({
   productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
-  ],
-  pwa: {
-    name: "DevFest India 2023",
-    workboxPluginMode: "InjectManifest",
-    themeColor: "#3D7FF5",
-    msTileColor: "#3D7FF5",
-    appleMobileWebAppCapable: "yes",
-    appleMobileWebAppStatusBarStyle: "black",
-    manifestOptions: {
-      background_color: "#ffffff",
-    },
-  }
+  ]
 })

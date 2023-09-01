5b38c4866c637410600ee5345bbd1defde472653
vrijraj <vrijraj2396@gmail.com>
2023-09-01 23:39:06 +0530

    pwa added

--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,13 @@ module.exports = defineConfig({
   productionSourceMap: false,
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  pwa:{
+    name: "DevFest India 2023",
+    themeColor: "#3D7FF5",
+    msTileColor: "#3D7FF5",
+    short_name: 'DevFest India',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black',
+  }
 })
